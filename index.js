ae2a412928d3147976607a11750c7a8ffa031eec
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-10-25 10:55:40 +0600

    fisrst commit

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
-const categories = require("./Datat/category.json");
+const categories = require("./data/category.json");
+const news = require("./data/news.json");
 
 app.use(cors());
 app.get("/", (req, res) => {
@@ -13,6 +14,27 @@ app.get("/", (req, res) => {
 
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
